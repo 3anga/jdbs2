199869b403df35b85adabb8894c25aa4d4faac7d
3anga <87752196+3anga@users.noreply.github.com>
2021-07-22 21:16:52 +0500

    /profile/v2/profiles GET/POST is ready for use

--- database.py
+++ database.py
@@ -6,12 +6,17 @@ from uuid import uuid4
 class DB:
     def __init__(self, **kwargs):
         self.dbname = kwargs["db"].split("/")[-1].split(".")[0]
-        self.session = sqlite3.connect(kwargs["db"], check_same_thread=False)
+        self.session = sqlite3.connect(kwargs["db"],
+                                       check_same_thread=False,
+                                       isolation_level=None)
         self.cursor = self.session.cursor()
 
     def __str__(self):
         return "<Database {"+self.dbname+"}>"
     
+    def __del__(self):
+        self.session.close()
+
     def getProfile(self, profileId = None):
         """
         Returns a profile from SQLite database
@@ -23,30 +28,30 @@ class DB:
                                     FROM profiles;''')
             profiles = self.cursor.fetchall()
         for profile in profiles:
-            if profile[-1].replace("'","") == profileId: return {
+            if profile[-1] == profileId: return {
                 "avatar": profile[0],
                 "skin": profile[1],
                 "jdPoints": profile[2],
-                "progression": loads(profile[3].replace("'","")),
-                "history": loads(profile[4].replace("'","")),
-                "favorites": loads(profile[5].replace("'","")),
-                "unlockedAvatars": loads(profile[6].replace("'","")),
-                "unlockedSkins": loads(profile[7].replace("'","")),
+                "progression": loads(profile[3]),
+                "history": loads(profile[4]),
+                "favorites": loads(profile[5]),
+                "unlockedAvatars": loads(profile[6]),
+                "unlockedSkins": loads(profile[7]),
                 "wdfRank": profile[8],
                 "stars": profile[9],
                 "unlocks": profile[10],
                 "songsPlayed": profile[11],
-                "name": profile[12].replace("'",""),
-                "nickname": profile[13].replace("'",""),
-                "platformId": profile[14].replace("'",""),
+                "name": profile[12],
+                "nickname": profile[13],
+                "platformId": profile[14],
                 "country": profile[15],
-                "scores": loads(profile[16].replace("'","")),
-                "language": profile[17].replace("'",""),
-                "unlockedPortraitBorders": loads(profile[18].replace("'","")),
+                "scores": loads(profile[16]),
+                "language": profile[17],
+                "unlockedPortraitBorders": loads(profile[18]),
                 "portraitBorder": profile[19],
-                "syncVersions": loads(profile[20].replace("'","")),
-                "otherPids": loads(profile[21].replace("'","")),
-                "mapHistory": loads(profile[22].replace("'","")),
+                "syncVersions": loads(profile[20]),
+                "otherPids": loads(profile[21]),
+                "mapHistory": loads(profile[22]),
                 "profileId": profileId,
             }
         return None
@@ -60,21 +65,17 @@ class DB:
         if userId is None: return False
 
         profileId = self.getUser(userId)["profileId"]
+        check = self.getProfile(profileId)
 
-        with self.session:
-            self.cursor.execute(f'''SELECT * 
-                            FROM profiles;''')
-            checks = self.cursor.fetchall()
-        for check in checks:
-            if check[-1].replace("'","") == profileId: return True
-        return False
+        if check is None: return False
+        return True
     
     def createProfile(self, userId = None, profile = None, xskuid = None):
         """
         Creates a profile using userId
         """
 
-        if userId or profile or xskuid is None: return None
+        #if userId or profile or xskuid is None: return None
 
         profile["profileId"] = self.getUser(userId)["profileId"]
 
@@ -87,7 +88,6 @@ class DB:
                 'timestamp': int(time() * 1000)
             }
         }
-        profile["platformId"] = xskuid_["platformId"]
 
         values = str()
         lastValue = list(profile.values())[-1]
@@ -97,7 +97,7 @@ class DB:
             elif type(value) == dict or list:
                 values += f"'{dumps(value)}'"
             else:
-                values += f"'{str(value)}'"
+                values += f"{str(value)}"
             
             if value != lastValue: values += ","
 
@@ -105,18 +105,24 @@ class DB:
             self.cursor.execute(f'''INSERT INTO profiles 
                             ({','.join(list(profile.keys()))})
                             VALUES ({values});''')
-            self.session.commit()
     
-    def updateProfile(self, userId=None, profile=None):
-        """
-        Updates a profile in SQLite database
-        """
-
-        if userId or profile is None: return
-
+    def updateProfile(self, userId = None, profile = None, xskuid = None):
         profileId = self.getUser(userId)["profileId"]
 
-        setinfo = str()
+        for xskuid_ in configuration.XSKU_IDS:
+            if xskuid_["code"] == xskuid: break
+
+        profile["syncVersions"] = self.getProfile(profileId)["syncVersions"]
+        if xskuid_["gameVersion"] in list(profile["syncVersions"].keys()):
+            profile["syncVersions"][xskuid_["gameVersion"]]["version"] += 1
+            profile["syncVersions"][xskuid_["gameVersion"]]["timestamp"] = int(time() * 1000)
+        else:
+            profile["syncVersions"][xskuid_["gameVersion"]] = {
+                'version': 0,
+                'timestamp': int(time() * 1000)
+            }
+
+        setinfo = ''
         lastValue = list(profile.values())[-1]
         for key, value in profile.items():
             setinfo += f"{key}="
@@ -126,16 +132,15 @@ class DB:
             elif type(value) == dict or list:
                 setinfo += f"'{dumps(value)}'"
             else:
-                setinfo += f"'{str(value)}'"
+                setinfo += f"{str(value)}"
 
             if value != lastValue: setinfo += ","
 
         with self.session:
             self.cursor.execute(f'''UPDATE profiles
-                            SET {setinfo}
-                            WHERE profileId={profileId};''')
-            self.session.commit()
-    
+                                    SET {setinfo}
+                                    WHERE profileId='{profileId}';''')
+
     def getUser(self, userId = None):
         """
         Returns a user
@@ -145,21 +150,22 @@ class DB:
 
         with self.session:
             self.cursor.execute(f'''SELECT * 
-                            FROM user
-                            WHERE userId={userId};''')
-            users = self.cursor.fetchone()
+                                    FROM users;''')
+            users = self.cursor.fetchall()
 
-        for user in users:
-            if user[0].replace("'","") == userId: return {
-                "userId": userId,
-                "profileId": user[1],
-                "platformCode": user[2],
-                user[2]: {
-                    "name": user[3],
-                    "email": user[4]
-                },
-                "discordId": user[5]
-            }
+        try:
+            for user in users:
+                if user[0] == userId: return {
+                        "userId": userId,
+                        "profileId": user[1],
+                        "platformCode": user[2],
+                        user[2]: {
+                            "name": user[3],
+                            "email": user[4]
+                        },
+                        "discordId": user[5]
+                    }
+        except Exception: pass
 
         return None
 
@@ -188,7 +194,6 @@ class DB:
                                 email,
                                 discordId
                             ))
-            self.session.commit()
     
     def updateUser(self, userId = None, **kwargs):
         """
@@ -205,7 +210,7 @@ class DB:
             elif type(value) == dict or list:
                 setinfo += f"'{dumps(value)}'"
             else:
-                setinfo += f"'{str(value)}'"
+                setinfo += f"{str(value)}"
 
             if value != lastValue: setinfo += ","
 
@@ -213,4 +218,3 @@ class DB:
             self.cursor.execute(f'''UPDATE users
                             SET {setinfo}
                             WHERE userId={userId};''')
-            self.session.commit()
--- index.py
+++ index.py
@@ -1,45 +1,51 @@
-import configuration
-from flask import Flask, request, abort
+from flask import Flask, request
 from database import DB
-from jwt import decode, encode
-from time import time
 from json import dumps
+import configuration 
 
-DATABASE = DB(db=configuration.DB_PATH)
 APP = Flask(__name__)
 
-#THIS WILL BE TEMPORARY OFFLINE 
-#@APP.before_request
-def before_request():
-    try:
-        if request.headers.get('User-Agent') in configuration.USER_AGENTS:
-            decodedAuthorization = decode(request.headers.get('Authorization'), 
-                                         configuration.PUBLIC_KEY,
-                                         algorithm=configuration.JWT_ALGORITHM)
-            if decodedAuthorization["banned"] == True or (time() *
-                                                           1000) > (decodedAuthorization["serverTime"] + 
-                                                                    decodedAuthorization["expiresIn"]):
-                return abort(401)
-            request.headers["Authorization"] = decodedAuthorization
-        else:
-            return abort(401)
-    except Exception:
-        return abort(401)
-
 @APP.route('/profile/v2/profiles', methods=['GET', 'POST'])
-def profiles():
+def profile():
     if request.method == 'GET':
-        response = []
-        for pid in request.args.get('profileIds').split(","):
-            response.append(DATABASE.getProfile(pid))
-        return dumps(response), 200, {"Content-Type": "application/json"}
+        DATABASE = DB(db=configuration.DB_PATH)
+
+        RESPONSE = []
+        for PID in request.args.get('profileIds').split(","):
+            RESPONSE.append(DATABASE.getProfile(PID))
+        
+        CONTENT = dumps(RESPONSE)
+        STATUS_CODE = 200
+        HEADERS = {
+            'Content-Type': 'application/json'
+        }
+
+        del DATABASE
+
+        return CONTENT, STATUS_CODE, HEADERS
     else:
-        userId = request.headers["Authorization"]["userId"]
-        if DATABASE.isProfileExists(userId) == True:
-            DATABASE.updateProfile(userId, request.get_json())
+        DATABASE = DB(db=configuration.DB_PATH)
+
+        #TODO: change to real data
+        USERID = 'd06b57e3-16a5-4828-b5a5-79de9e649841'
+
+        BODY = request.get_json()
+        XSKUID = request.headers.get('X-SkuId')
+
+        if DATABASE.isProfileExists(USERID) == True:
+            DATABASE.updateProfile(userId=USERID, profile=BODY)
         else:
-            DATABASE.createProfile(userId, request.get_json())
-        return 200
+            DATABASE.createProfile(userId=USERID, profile=BODY, xskuid=XSKUID)
+
+        CONTENT = dumps([])
+        STATUS_CODE = 200
+        HEADERS = {
+            'Content-Type': 'application/json'
+        }
+
+        del DATABASE
+
+        return CONTENT, STATUS_CODE, HEADERS
 
 if __name__ == "__main__":
     APP.run()
